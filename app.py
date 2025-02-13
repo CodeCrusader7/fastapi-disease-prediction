5868e62335f06f5f12b0bc60b407f880554f89ed
CodeCrusader7 <126020130+CodeCrusader7@users.noreply.github.com>
2025-02-13 21:31:40 +0500

    Fixed model path issue for Render deployment

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import joblib
+import os
 import pandas as pd
 
 app = FastAPI()
 
-# Load Model and Label Encoders
-model = joblib.load(r"C:\Users\lenovo\Documents\FastAPI_Project\disease_prediction_model.pkl")
+# Get the absolute path of the current file
+BASE_DIR = os.path.dirname(__file__)
+
+# Define the model path
+MODEL_PATH = os.path.join(BASE_DIR, "disease_prediction_model.pkl")
+
+# Load the model
+model = joblib.load(MODEL_PATH)
+
 label_encoders = joblib.load(r"C:\Users\lenovo\Documents\FastAPI_Project\label_encoders.pkl")
 
 @app.post("/predict/")
